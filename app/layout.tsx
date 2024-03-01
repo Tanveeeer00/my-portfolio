cff8b759942c741372172ec69f4314aebfe2a513
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-01 06:26:41 +0530

    dark mode switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import ActiveSectionContextProvider from "@/context/ActiveSectionContextProvider";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,16 +22,26 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} h-[100%] bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+        className={`${inter.className} h-[100%] bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-slate-900 dark:text-white`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25] rounded-full blur-[10rem] sm:w-[68.rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[35rem] h-[31.25rem] w-[50.25] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div
+          className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-1/4 w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.rem]
+        dark:bg-sky-700 dark:blur-[15rem] dark:bg-opacity-100 dark:bg-backdrop-blur-[5rem] dark:-z-10  
+        "
+        ></div>
+        <div
+          className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[35rem] h-1/4 w-[50.25] rounded-full blur-[10rem] 
+        sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
+         dark:bg-slate-800 dark:blur-[5rem] dark:bg-opacity-80 dark:bg-backdrop-blur-[3rem]
+         "
+        ></div>
         <ActiveSectionContextProvider>
           <Header />
           {children}
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+        <ThemeSwitcher />
       </body>
     </html>
   );
