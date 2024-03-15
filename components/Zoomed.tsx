8531ffa9e9bad52b33c066f59dc85b81c4a1f7b9
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-16 03:21:42 +0530

    zoomed image

---
+++ components/Zoomed.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useRef, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+
+interface ImageProps {
+  src: string | StaticImageData;
+  alt: string;
+}
+export default function Zoomed({ src, alt }: ImageProps) {
+  const [isZoomed, setIsZoomed] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  const handleZoomClick = () => {
+    setIsZoomed(!isZoomed);
+  };
+
+  return (
+    <div className="image-container">
+      <Image
+        src={src}
+        alt={alt}
+        quality={95}
+        className={`cursor-pointer ${
+          isZoomed ? "w-full h-full fixed top-0 left-0 z-50" : ""
+        }`}
+        onClick={handleZoomClick}
+        ref={imageRef}
+      />
+      {isZoomed && ( // Conditionally render zoom overlay
+        <div
+          className="zoom-overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-40"
+          onClick={handleZoomClick}
+        />
+      )}
+    </div>
+  );
+}
