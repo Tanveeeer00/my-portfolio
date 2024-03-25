e14785c51544e8636e195183bd7996be431057c4
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-26 00:48:46 +0530

    some changes in app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import ActiveSectionContextProvider from "@/context/ActiveSectionContextProvider";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
@@ -38,7 +38,7 @@ export default function RootLayout({
         ></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
-            <Header />
+            {/* <Header /> */}
             {children}
             <Footer />
             <Toaster position="top-right" />
--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
 import Skills from "@/components/Skills";
 import Overview from "@/components/Overview";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
+      <Header />
       <Intro />
       <SectionDivider />
       <About />
